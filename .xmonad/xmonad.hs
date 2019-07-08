31f1aebf1dfba11aa019464cb4b2a905bad3b8c1
Scott Godbold <scgodbold@gmail.com>
2019-07-08 16:26:38 -0400

    Add some custom keybindings to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 -- default desktop configuration for Fedora
 
+import qualified Data.Map as M
 import System.IO
 import System.Posix.Env (getEnv)
 
@@ -7,13 +8,16 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Spacing
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Run(safeSpawn, spawnPipe)
+
+term = "/usr/local/bin/alacritty"
 
 main = do
   xmproc <- spawnPipe "xmobar"
   session <- getEnv "DESKTOP_SESSION"
   xmonad  $ desktopConfig {
     terminal = "/usr/local/bin/alacritty"
+    , keys = myKeys
     , borderWidth = 0
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
@@ -24,3 +28,11 @@ main = do
   }
 
 desktop _ = desktopConfig
+
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+  [ ((modMask,      xK_z     ), safeSpawn "slock" [])
+  , ((modMask,      xK_Return), safeSpawn (XMonad.terminal conf) [])
+  , ((modMask,      xK_c     ), kill)
+  , ((modMask,      xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+  ]
